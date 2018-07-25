b7343e82d91f3880761b87297ae1b2a79a41ad4e
0v1se <dev@0v1se.com>
2018-07-25 10:12:28 +0300

    updated regulated lib

--- contracts/KycProviderFactory.sol
+++ contracts/KycProviderFactory.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.24;
 
 import "@daonomic/regulated/contracts/KycProviderImpl.sol";
+import "@daonomic/regulated/contracts/WhitelistKycProvider.sol";
+
 
 contract KycProviderFactory {
     event KycProviderCreated(address addr);
